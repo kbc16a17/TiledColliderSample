c578e842c42881fe6847fe4ee7fcffc28e7b28f3
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2018-06-14 16:56:40 +0900

    Initial Commit

---
+++ Assets/Scripts/SampleScene/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SampleScene {
+    public class GameManager : MonoBehaviour {
+        public GameObject Prefab;
+
+        private void Update() {
+            if (Input.GetMouseButtonDown(0)) {
+                var point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                point.z = 0.0f;
+                Instantiate(Prefab, point, Quaternion.identity);
+            }
+        }
+    }
+}
